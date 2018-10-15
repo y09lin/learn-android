49a6aceffb1cbb6e7a09da9ce795123eb57d5e95
huim_lin <huim_lin@haihangyun.com>
2018-10-15 09:52:52 +0800

    添加刷新控件

--- app/src/main/java/com/huim_lin/learn/activity/MainActivity.java
+++ app/src/main/java/com/huim_lin/learn/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.huim_lin.learn.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
@@ -11,6 +12,7 @@ import com.huim_lin.learn.bean.Article;
 import com.huim_lin.learn.bean.PageDto;
 import com.huim_lin.learn.util.RequestUtil;
 import com.huim_lin.learn.util.ToastUtil;
+import com.huim_lin.learn.widget.SuperSwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ArticleAdapter articleAdapter;
     private List<Article> list;
+    private SuperSwipeRefreshLayout swipeLayout;
     private int pageIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        swipeLayout = findViewById(R.id.swipe_layout);
         listView = findViewById(R.id.list);
         list = new ArrayList<>();
         articleAdapter = new ArticleAdapter(this,list,R.layout.item_article);
@@ -37,6 +41,43 @@ public class MainActivity extends AppCompatActivity {
 
     private void initData() {
         pageIndex = 0;
+        swipeLayout.setOnPullRefreshListener(new SuperSwipeRefreshLayout.OnPullRefreshListener() {
+            @Override
+            public void onRefresh() {
+                pageIndex = 0;
+                list.clear();
+                getData();
+            }
+
+            @Override
+            public void onPullDistance(int distance) {
+            }
+
+            @Override
+            public void onPullEnable(boolean enable) {
+            }
+
+        });
+        swipeLayout.setOnPushLoadMoreListener(new SuperSwipeRefreshLayout.OnPushLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                pageIndex++;
+                getData();
+            }
+
+            @Override
+            public void onPushDistance(int distance) {
+            }
+
+            @Override
+            public void onPushEnable(boolean enable) {
+            }
+        });
+    }
+
+    private void endSwipe(){
+        swipeLayout.setRefreshing(false);
+        swipeLayout.setLoadMore(false);
     }
 
     public void getData() {
@@ -46,12 +87,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onGetList: " + page.getTotalPage() + " " + page.getTotalElement());
                 list.addAll(page.getDataList());
                 articleAdapter.notifyDataSetHasChanged();
+                endSwipe();
             }
 
             @Override
             public void onError(long code) {
                 Log.d(TAG,"onError");
                 ToastUtil.showText(String.format(getString(R.string.requst_error),code));
+                endSwipe();
             }
         });
     }
